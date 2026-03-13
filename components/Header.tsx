2a49f48ac679a29bcf64fb631a531689dac7f574
shreyash1231 <chandwadkarshreyash@gmail.com>
2026-03-13 11:08:45 +0530

    feat: initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+export default function Header() {
+  return (
+    <div className="px-20 py-20 z-50">
+        <div className="bg-[#dcd8ce] p-4 rounded-4xl">
+            <div className="grid grid-cols-4">
+              <div className="col-span-3">
+                <div className="flex gap-95">
+                  <div>
+
+                  </div>
+                  <div className="flex gap-10 text-black">
+                    <div>About</div>
+                    <div>Programs</div>
+                    <div>Corporte</div>
+                    <div>Blog</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+  );
+}
